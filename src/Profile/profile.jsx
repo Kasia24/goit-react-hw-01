7a530e409b6ce6e6d14b10ab36e7fb94079ba4f0
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-10-27 16:03:20 +0100

    add

---
+++ src/Profile/profile.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Profile = ({ name, tag, location, image, stats }) => {
+  return (
+    <div className="profile">
+      <div className="profile-info">
+        <img
+          src={
+            image || "https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          }
+          alt="User avatar"
+          className="profile-avatar"
+        />
+        <p className="profile-name">{name}</p>
+        <p className="profile-tag">@{tag}</p>
+        <p className="profile-location">{location}</p>
+      </div>
+
+      <ul className="profile-stats">
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default Profile;
